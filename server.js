7003c1c239fd7006710a2a9b7a36720cc7434209
woori3104 <sizuku11261@gmail.com>
2021-07-12 00:56:45 +0900

    prisma install

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
-const { ApolloServer, gql } = require("apollo-server");
+import { ApolloServer, gql } from "apollo-server";
 
  const typeDefs = gql`
+   type Movie {
+     title: String
+     year: Int
+   }
    type Query {
-     hello: String
+     movies: [Movie]
+     movie: Movie
+   }
+   type Mutation {
+     createMovie(title: String!): Boolean
+     deleteMovie(title: String!): Boolean
    }
  `;
 
  const resolvers = {
    Query: {
-     hello: () => "bebe",
+     movies: () => [],
+     movie: () => ({ title: "Hello", year: 2021 }),
+   },
+   Mutation: {
+     createMovie: (_, { title }) => {
+       console.log(title);
+       return true;
+     },
+     deleteMovie: (_, { title }) => {
+       console.log(title);
+       return true;
+     },
    },
  };
-
- const server = new ApolloServer({
-   typeDefs,
-   resolvers,
- });
-
- server
-   .listen()
-   .then(() => console.log("Server is running on http://localhost:4000/"));
