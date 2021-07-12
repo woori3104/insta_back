97099bf10d0e1b6174cd45b18e4136f0fd6c8fed
woori3104 <sizuku11261@gmail.com>
2021-07-12 11:37:06 +0900

    PRISMA 세팅

--- server.js
+++ server.js
@@ -1,33 +1,53 @@
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 import { ApolloServer, gql } from "apollo-server";
 
- const typeDefs = gql`
-   type Movie {
-     title: String
-     year: Int
-   }
-   type Query {
-     movies: [Movie]
-     movie: Movie
-   }
-   type Mutation {
-     createMovie(title: String!): Boolean
-     deleteMovie(title: String!): Boolean
-   }
- `;
+const client = new PrismaClient();
 
- const resolvers = {
-   Query: {
-     movies: () => [],
-     movie: () => ({ title: "Hello", year: 2021 }),
-   },
-   Mutation: {
-     createMovie: (_, { title }) => {
-       console.log(title);
-       return true;
-     },
-     deleteMovie: (_, { title }) => {
-       console.log(title);
-       return true;
-     },
-   },
- };
+const typeDefs = gql`
+  type movie {
+    id: Int!
+    title: String!
+    year: Int!
+    genre: String
+    createdAt: String!
+    updatedAt: String!
+  }
+  type Query {
+    movies: [movie]
+    movie(id: Int!): movie
+  }
+  type Mutation {
+    createMovie(title: String!, year: Int!, genre: String): movie
+    deleteMovie(id: String!): Boolean
+  }
+`;
+
+const resolvers = {
+  Query: {
+    movies: () => client.movie.findMany(),
+    movie: (_, { id }) => ({ title: "Hello", year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (_, { title, year, genre }) =>
+      client.movie.create({
+        data: {
+          title,
+          year,
+          genre,
+        },
+      }),
+    deleteMovie: (_, { id }) => {
+      return true;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server
+  .listen()
+  .then(() => console.log("Server is running on http://localhost:4000/"));
